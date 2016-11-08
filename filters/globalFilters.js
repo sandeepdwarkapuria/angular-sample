0f6c285fba1e1f6e73fd77e45cee99b10affe3b3
Ankit <ankit@techsarksit.com>
2016-11-08 10:42:56 +0530

    deleted

--- filters/globalFilters.js
+++
@@ -1,8 +0,0 @@
-angular.module('app')
-
-	// {{ input | int:option:option2 }}
-    .filter('int',function () {
-        return function (input,option,option2) {
-            return input;
-        }
-    });
